961c2e8f7f16c2d0f9de9318de1374d676ebb0db
BiniyamNebso <biniyamnebso@gmail.com>
2024-07-02 17:37:12 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // Set the base URL for your deployment
   plugins: [react()],
-
   css: {
-
-modules:{
-  localsConvention: "camelCase",
-},
+    modules: {
+      localsConvention: "camelCase",
+    },
   }
 })
